73cb76bc178408eab09eeaa2e6eee3652e545888
Said1703 <said-drk@hotmail.com>
2024-01-20 18:58:38 -0500

    agregando validacion

--- information/serializers.py
+++ information/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import ModelSerializer
 from .models import Information, Paises, Autos
-from django.contrib.auth import authenticate
+# from django.contrib.auth import authenticate
 
 
 class InformationSerializer(ModelSerializer):
@@ -9,23 +9,23 @@ class InformationSerializer(ModelSerializer):
         model = Information
         fields = "__all__"
 
-    def create(self, validated_data):
-        numero_doc = validated_data.get('numero_doc')
-        tipo_documento = validated_data.get('tipo_documento')
-        placa_vehiculo = validated_data.get('placa_vehiculo')
+    # def create(self, validated_data):
+    #     numero_doc = validated_data.get('numero_doc')
+    #     tipo_documento = validated_data.get('tipo_documento')
+    #     placa_vehiculo = validated_data.get('placa_vehiculo')
 
-        user = authenticate(request=self.context.get('request'),
-                            numero_doc=numero_doc,
-                            tipo_documento=tipo_documento,
-                            placa_vehiculo=placa_vehiculo)
+    #     user = authenticate(request=self.context.get('request'),
+    #                         numero_doc=numero_doc,
+    #                         tipo_documento=tipo_documento,
+    #                         placa_vehiculo=placa_vehiculo)
 
-        if not user or not user.is_authenticated:
-            raise ValidationError(
-                "Autenticación fallida. Verifica tus credenciales.")
+    #     if not user or not user.is_authenticated:
+    #         raise ValidationError(
+    #             "Autenticación fallida")
 
-        information_instance = Information.objects.create(**validated_data)
+    #     information_instance = Information.objects.create(**validated_data)
 
-        return information_instance
+    #     return information_instance
 
 
 class PaisesSerializer(ModelSerializer):
--- information/views.py
+++ information/views.py
@@ -1,13 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Information, Paises, Autos
 from .serializers import InformationSerializer, PaisesSerializer, AutosSerializer
-from rest_framework.permissions import IsAuthenticated
+from django.views import View
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+# from rest_framework.permissions import IsAuthenticated
 
 
 class InformationViewSet(ModelViewSet):
     queryset = Information.objects.all()
     serializer_class = InformationSerializer
-    pagintation_class = [IsAuthenticated]
+    # pagintation_class = [IsAuthenticated]
 
 
 class PaisesViewSet(ModelViewSet):
@@ -18,3 +21,13 @@ class PaisesViewSet(ModelViewSet):
 class AutosViewSet(ModelViewSet):
     queryset = Autos.objects.all()
     serializer_class = AutosSerializer
+
+
+@api_view(["POST"])
+def login(request):
+    validation = Information.objects.filter(numero_doc=request.data["numero_doc"]).filter(
+        tipo_documento=request.data["tipo_documento"]).filter(placa_vehiculo=request.data["placa_vehiculo"])
+
+    serializer = InformationSerializer(validation, many=True)
+
+    return JsonResponse({"ok": True, "data": serializer.data})
